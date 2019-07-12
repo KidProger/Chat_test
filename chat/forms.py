c73f83f4f5dbabcfdfd7e7967963827d848417fe
Kolya <kolya.bessmertny@gmail.com>
2019-07-12 10:42:46 +0300

    Fix labels name

--- chat/forms.py
+++ chat/forms.py
@@ -1,5 +1,7 @@
 from django import forms
 
+from django.utils.translation import gettext_lazy as _
+
 from .models import MessageModel, ChatModel
 
 class MessageForm(forms.Form):
@@ -8,10 +10,13 @@ class MessageForm(forms.Form):
 
 
 class ChatCreateForm(forms.ModelForm):
-    user = forms.CharField(max_length=30)
+    user = forms.CharField(max_length=30, label="User name")
     class Meta:
         model = ChatModel
         fields = ['name']
+        labels = {
+            'name': _('Chat name'),
+        }
 
 
 
